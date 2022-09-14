cb8aa633587e2264b6f590113abb639004530556
Anders Borch <anders@cyborch.com>
2022-09-14 10:56:54 +0200

    Added all source dependencies to build target

--- scripts/build.mk
+++ scripts/build.mk
@@ -1,10 +1,12 @@
 .PHONY: corp corp-clean
 
+GO_SOURCES := $(shell find $(ROOT_DIR)cmd $(ROOT_DIR)internal -name '*.go')
+
 corp: $(ROOT_DIR)bin/corp
 
-$(ROOT_DIR)bin/corp: $(wildcard cmd/corp/*.go)
-	mkdir -p $(ROOT_DIR)
-	go build -o $(ROOT_DIR)bin/corp $<
+$(ROOT_DIR)bin/corp: $(GO_SOURCES)
+	mkdir -p $(ROOT_DIR)bin
+	go build -o $(ROOT_DIR)bin/corp $(wildcard cmd/corp/*.go)
 
 corp-clean:
 	if [ $(ROOT_DIR) == "" ] ; then exit 1 ; fi
